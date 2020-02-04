15945cb669fa101b2f168a1d4387ce3e3b371d3f
Sergey Pererva <proger.su@gmail.com>
2020-02-04 13:51:30 +0200

    Add typography? change file url function

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@ add_action( 'wp_enqueue_scripts', '_action_starter_enqueue_scripts', 9999 );
 
 function _action_starter_enqueue_scripts() {
     $theme = wp_get_theme();
-    wp_enqueue_style( 'starter', get_template_directory_uri() . '/static/css/styles.css', array(), $theme->get( 'Version' ) );
+    wp_enqueue_style( 'starter', get_theme_file_uri('static/css/styles.css'), array(), $theme->get( 'Version' ) );
 
-    wp_enqueue_script( 'starter', get_template_directory_uri() . '/static/js/scripts.js', array( 'jquery', 'imagesloaded' ), $theme->get( 'Version' ), true );
+    wp_enqueue_script( 'starter', get_theme_file_uri('static/js/scripts.js'), array( 'jquery', 'imagesloaded' ), $theme->get( 'Version' ), true );
 
     wp_localize_script( 'starter', 'starterParams', array(
         'homeUrl'  => home_url( '/' ),
